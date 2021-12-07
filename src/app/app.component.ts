ed8a20e4718b98366a8b537a174a5631f08c9702
ziad yahya <ziadyahya01001@gmail.com>
2021-12-07 18:12:06 +0200

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Direction } from '@angular/cdk/bidi';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'task';
+export class AppComponent implements OnInit{
+  appDirection:Direction = 'ltr'
+  constructor(){}
+  ngOnInit(){
+
+  }
+
+  toggleDirection(){
+    if(this.appDirection === 'ltr'){
+      this.appDirection = 'rtl'
+      console.log(this.appDirection );
+    }else {
+      this.appDirection = 'ltr';
+      console.log('hii');
+    }
+  }
 }
